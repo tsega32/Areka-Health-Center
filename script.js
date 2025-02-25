617ce0afbe0a2ab0d950ec877ca7c98270b66706
tsega32 <62291635+tsega32@users.noreply.github.com>
2025-02-25 16:19:31 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+// Simple form submission handling
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Thank you for contacting us. We will get back to you shortly.');
+    // Here you can add code to send the form data to a server
+});
